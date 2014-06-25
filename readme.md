3388ab2ec69b0ad141db4fa44d8390f14e210d7f
Ross McKay <rmckay@webaware.com.au>
2014-06-25 15:46:51 +1000

    fix affiliate link in requires GF message; add to readme.md

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Gravity Forms eWAY #
 
-Integrate Gravity Forms with the eWAY credit card payment gateway
+Integrate [Gravity Forms](https://www.e-junkie.com/ecom/gb.php?cl=54585&c=ib&aff=277327) with the eWAY credit card payment gateway
 
 * [Home](http://shop.webaware.com.au/downloads/gravity-forms-eway/)
 * [GitHub](https://github.com/webaware/gravityforms-eway/)
--- views/requires-gravity-forms.php
+++ views/requires-gravity-forms.php
@@ -1,4 +1,4 @@
 
 <div class="error">
-	<p>Gravity Forms eWAY requires <a target="_blank" href="https://www.e-junkie.com/ecom/gb.php?cl=54585&c=ib&aff=277327">Gravity Forms</a> to be installed and activated.</p>
+	<p>Gravity Forms eWAY requires <a target="_blank" href="https://www.e-junkie.com/ecom/gb.php?cl=54585&amp;c=ib&amp;aff=277327">Gravity Forms</a> to be installed and activated.</p>
 </div>
