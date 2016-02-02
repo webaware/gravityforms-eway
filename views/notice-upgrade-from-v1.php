08b652025268d71894e7bf614773ee1d9fc3e249
Ross McKay <rmckay@webaware.com.au>
2016-02-02 12:15:29 +1100

    improve wording on v1 upgrade notice

--- views/notice-upgrade-from-v1.php
+++ views/notice-upgrade-from-v1.php
@@ -1,10 +1,10 @@
 
 <div class="error gfeway-dismissable" data-gfeway-dismiss="upgrade_from_v1">
-	<p><?php _e('Gravity Forms eWAY has been upgraded. Please ensure check your settings.', 'gravityforms-eway'); ?></p>
+	<p><?php _e('Gravity Forms eWAY has been upgraded. Please check your settings.', 'gravityforms-eway'); ?></p>
 
 	<ul style="padding-left: 2em">
 		<?php if (empty($options['apiKey']) && empty($options['apiPassword'])):  ?>
-		<li style="list-style-type:disc"><?php printf(__('please enter your eWAY API key and password in <a href="%s">eWAY Payments settings</a> to upgrade to Rapid API', 'gravityforms-eway'), esc_url($ewaySettingsURL)); ?></li>
+		<li style="list-style-type:disc"><?php printf(__('upgrade to Rapid API by entering your eWAY API key and password, and Client Side Encryption Key, at <a href="%s">eWAY Payments settings</a>', 'gravityforms-eway'), esc_url($ewaySettingsURL)); ?></li>
 		<?php endif; ?>
 		<li style="list-style-type:disc"><?php printf(__('currencies other than AUD are now supported; please check the <a href="%s">currency selected for Gravity Forms</a>', 'gravityforms-eway'), esc_url($gfSettingsURL)); ?></li>
 		<li style="list-style-type:disc"><?php _e('accepted credit cards are no longer restricted; please check your forms for accepted credit cards', 'gravityforms-eway'); ?></li>
