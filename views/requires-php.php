2113c3d3b3fc8d7f1ab06adbfd61fb331c32058d
Ross McKay <rmckay@webaware.com.au>
2017-09-19 14:18:44 +1000

    bump min recommended PHP to 7.0

--- views/requires-php.php
+++ views/requires-php.php
@@ -2,5 +2,5 @@
 <div class="error">
 	<p><?php printf(__('Gravity Forms eWAY requires PHP %1$s or higher; your website has PHP %2$s which is <a target="_blank" href="%3$s">old, obsolete, and unsupported</a>.', 'gravityforms-eway'),
 			esc_html($php_min), esc_html(PHP_VERSION), 'http://php.net/eol.php'); ?></p>
-	<p><?php printf(__('Please upgrade your website hosting. At least PHP %s is recommended.', 'gravityforms-eway'), '5.5'); ?></p>
+	<p><?php printf(__('Please upgrade your website hosting. At least PHP %s is recommended.', 'gravityforms-eway'), '7.0'); ?></p>
 </div>
