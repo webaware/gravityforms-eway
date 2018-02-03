134f6608dbd1b6ef8ffb35c5844a129401be168e
Ross McKay <rmckay@webaware.com.au>
2018-02-03 13:28:09 +1100

    update prerequisite warning notices for css classes, PHP version rec.

--- views/requires-gravity-forms-upgrade.php
+++ views/requires-gravity-forms-upgrade.php
@@ -1,5 +1,10 @@
+<?php
+if (!defined('ABSPATH')) {
+	exit;
+}
+?>
 
-<div class="error">
+<div class="notice notice-error">
 	<p><?php printf(__('Gravity Forms eWAY requires <a rel="noopener" target="_blank" href="%1$s">Gravity Forms</a> version %2$s or higher; your website has Gravity Forms version %3$s', 'gravityforms-eway'),
 		'https://webaware.com.au/get-gravity-forms', esc_html(GFEwayPlugin::MIN_VERSION_GF), esc_html(GFCommon::$version)); ?></p>
 </div>
--- views/requires-php.php
+++ views/requires-php.php
@@ -1,6 +1,11 @@
+<?php
+if (!defined('ABSPATH')) {
+	exit;
+}
+?>
 
-<div class="error">
+<div class="notice notice-error">
 	<p><?php printf(__('Gravity Forms eWAY requires PHP %1$s or higher; your website has PHP %2$s which is <a rel="noopener" target="_blank" href="%3$s">old, obsolete, and unsupported</a>.', 'gravityforms-eway'),
 			esc_html($php_min), esc_html(PHP_VERSION), 'http://php.net/eol.php'); ?></p>
-	<p><?php printf(__('Please upgrade your website hosting. At least PHP %s is recommended.', 'gravityforms-eway'), '7.0'); ?></p>
+	<p><?php printf(__('Please upgrade your website hosting. At least PHP %s is recommended.', 'gravityforms-eway'), '7.1'); ?></p>
 </div>
