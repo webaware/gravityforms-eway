db4576c590049c22c70606316f9c059a8e3228a9
Ross McKay <rmckay@webaware.com.au>
2023-04-30 17:41:27 +1000

    compile css / js

--- static/js/gfeway_ecrypt.js
+++ static/js/gfeway_ecrypt.js
@@ -1,4 +1,5 @@
 // gravityforms-eway: Client Side Encryption
+
 (function ($) {
   /**
   * if form has Client Side Encryption key, hook its submit action for maybe encrypting
@@ -8,54 +9,47 @@
   $(document).on("gform_post_render", function (event, form_id) {
     $("#gform_" + form_id + "[data-gfeway-encrypt-key]").on("submit", maybeEncryptForm);
   });
+
   /**
   * use ES6 String.prototype.repeat() if available, providing fallback for IE11
   * @param {String} character
   * @param {Number}
   * @return {String}
   */
-
-  var repeatString = function () {
+  const repeatString = function () {
     if (typeof String.prototype.repeat === "function") {
-      return function (character, length) {
+      return (character, length) => {
         return character.repeat(length);
       };
     }
-
-    return function (character, length) {
-      var s = character;
-
-      for (var i = 1; i < length; i++) {
+    return (character, length) => {
+      let s = character;
+      for (let i = 1; i < length; i++) {
         s += character;
       }
-
       return s;
     };
   }();
+
   /**
   * check form for conditions to encrypt sensitive fields
   * @param {jQuery.Event} event
   */
-
-
   function maybeEncryptForm(event) {
-    var frm = $(this);
-    var form_id = extractFormId(this.id); // don't encrypt if sending to the Recurring Payment XML API
+    const frm = $(this);
+    const form_id = extractFormId(this.id);
 
+    // don't encrypt if sending to the Recurring Payment XML API
     if (frm.find(".gfeway-recurring-active").length) {
       return true;
     }
-
-    var key = frm.data("gfeway-encrypt-key");
-
+    const key = frm.data("gfeway-encrypt-key");
     function maybeEncryptField(field_selector) {
-      var field = frm.find(field_selector);
-
+      const field = frm.find(field_selector);
       if (field.length) {
-        var value = field.val().trim();
-        var length = value.length;
-        var target = field.data("gfeway-encrypt-name");
-
+        const value = field.val().trim();
+        const length = value.length;
+        const target = field.data("gfeway-encrypt-name");
         if (target === "EWAY_CARDNUMBER" && !cardnumberValid(value)) {
           throw {
             name: "Credit Card Error",
@@ -63,20 +57,17 @@
             field: field
           };
         }
-
         if (length) {
-          var encrypted = eCrypt.encryptValue(field.val(), key);
+          const encrypted = eCrypt.encryptValue(field.val(), key);
           $("<input type='hidden'>").attr("name", target).val(encrypted).appendTo(frm);
           field.val("").prop("placeholder", repeatString(gfeway_ecrypt_strings.ecrypt_mask, length));
         }
       }
     }
-
     function extractFormId(form_element_id) {
-      var parts = form_element_id.split("_");
+      const parts = form_element_id.split("_");
       return parts.length > 1 ? parts[1] : "";
     }
-
     try {
       maybeEncryptField("input[data-gfeway-encrypt-name='EWAY_CARDNUMBER']");
       maybeEncryptField("input[data-gfeway-encrypt-name='EWAY_CARDCVN']");
@@ -87,32 +78,30 @@
       e.field.focus();
       window.alert(e.message);
     }
-
     return true;
   }
+
   /**
   * basic card number validation using Luhn algorithm
   * @param {String} card_number
   * @return bool
   */
-
-
   function cardnumberValid(card_number) {
-    var checksum = 0;
-    var multiplier = 1; // process each character, starting at the right
+    let checksum = 0;
+    let multiplier = 1;
 
-    for (var i = card_number.length - 1; i >= 0; i--) {
-      var digit = card_number.charAt(i) * multiplier;
-      multiplier = multiplier === 1 ? 2 : 1; // digit can't be greater than 9
+    // process each character, starting at the right
+    for (let i = card_number.length - 1; i >= 0; i--) {
+      let digit = card_number.charAt(i) * multiplier;
+      multiplier = multiplier === 1 ? 2 : 1;
 
+      // digit can't be greater than 9
       if (digit >= 10) {
         checksum++;
         digit -= 10;
       }
-
       checksum += digit;
     }
-
     return checksum % 10 === 0;
   }
 })(jQuery);
--- static/js/recurring.js
+++ static/js/recurring.js
@@ -1,62 +1,61 @@
 // gravityforms-eway: Recurring Payments field
+
 // initialise form on page load
 (function ($) {
-  var thisYear = new Date().getFullYear();
-  var yearRange = thisYear + ":2099"; // year range for max date settings; mumble mumble jquery-ui mumble
+  const thisYear = new Date().getFullYear();
+  const yearRange = thisYear + ":2099"; // year range for max date settings; mumble mumble jquery-ui mumble
+  const reDatePattern = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/; // regex test for ISO date string
+  let setPickerOptions = false;
 
-  var reDatePattern = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/; // regex test for ISO date string
-
-  var setPickerOptions = false; // set datepicker minimum date if given
-
+  // set datepicker minimum date if given
   $("input[data-gfeway-minDate]").each(function () {
-    var input = $(this);
-    var minDate = this.getAttribute("data-gfeway-minDate"); // if minDate is an ISO date string, convert to a Date object as a reliable way to set minDate
+    const input = $(this);
+    let minDate = this.getAttribute("data-gfeway-minDate");
 
+    // if minDate is an ISO date string, convert to a Date object as a reliable way to set minDate
     if (reDatePattern.test(minDate)) {
       minDate = new Date(minDate);
     }
-
     input.datepicker("option", "minDate", minDate);
     setPickerOptions = true;
-  }); // set datepicker maximum date if given
+  });
 
+  // set datepicker maximum date if given
   $("input[data-gfeway-maxDate]").each(function () {
-    var input = $(this);
-    var maxDate = this.getAttribute("data-gfeway-maxDate"); // if maxDate is an ISO date string, convert to a Date object as a reliable way to set maxDate
+    const input = $(this);
+    let maxDate = this.getAttribute("data-gfeway-maxDate");
 
+    // if maxDate is an ISO date string, convert to a Date object as a reliable way to set maxDate
     if (reDatePattern.test(maxDate)) {
       maxDate = new Date(maxDate);
     }
-
     input.datepicker("option", "yearRange", yearRange); // need to reset year range so can extend max date!
-
     input.datepicker("option", "maxDate", maxDate);
     setPickerOptions = true;
-  }); // hack: setting options on datepicker fields after initialisation makes the datepicker div show at the bottom of the page
+  });
 
+  // hack: setting options on datepicker fields after initialisation makes the datepicker div show at the bottom of the page
   if (setPickerOptions) {
     $("#ui-datepicker-div").hide();
   }
+
   /**
   * set recurring field to active, until otherwise advised by conditional logic triggers
   * @param {jQuery.Event} event
   * @param {Number} form_id int ID of Gravity Forms form
   */
-
-
   $(document).on("gform_post_render", function (event, form_id) {
     $("#gform_" + form_id + " .gfeway-contains-recurring").addClass("gfeway-recurring-active");
-  }); // watch for conditional logic changes
+  });
 
-  gform.addAction("gform_post_conditional_logic_field_action", function (formId, action, targetId
-  /* , defaultValues, isInit */
-  ) {
-    var target = $(targetId);
-
+  // watch for conditional logic changes
+  gform.addAction("gform_post_conditional_logic_field_action", function (formId, action, targetId /* , defaultValues, isInit */) {
+    const target = $(targetId);
     if (target.hasClass("gfeway-contains-recurring")) {
       if (action === "show") {
-        target.addClass("gfeway-recurring-active").removeClass("gfeway-recurring-inactive"); // reset fields to initial values
+        target.addClass("gfeway-recurring-active").removeClass("gfeway-recurring-inactive");
 
+        // reset fields to initial values
         target.find("input.datepicker").each(function () {
           this.value = this.getAttribute("value");
         });
